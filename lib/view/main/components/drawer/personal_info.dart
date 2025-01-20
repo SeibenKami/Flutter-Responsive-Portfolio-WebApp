2c4e38823f3422a89a718141980efdfc5a9da043
Frank Adu <adufrank03@gmail.com>
2025-01-20 05:16:02 +0000

    url fixes

--- lib/view/main/components/drawer/personal_info.dart
+++ lib/view/main/components/drawer/personal_info.dart
@@ -11,15 +11,20 @@ class PersonalInfo extends StatelessWidget {
     return const Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        SizedBox(height: defaultPadding/2,),
-        AreaInfoText(title: 'Contact', text: '03166657602'),
-        AreaInfoText(title: 'Email', text: 'rh676838@gmail.com'),
-        AreaInfoText(title: 'LinkedIn', text: '@hamad-anwar'),
-        AreaInfoText(title: 'Github', text: '@hamad-anwar'),
+        SizedBox(
+          height: defaultPadding / 2,
+        ),
+        AreaInfoText(title: 'Contact', text: '+233 544 054 537'),
+        AreaInfoText(title: 'Email', text: 'adufrank03@gmail.com'),
+        AreaInfoText(title: 'LinkedIn', text: '@frank-adu-945561168'),
+        AreaInfoText(title: 'Github', text: '@SeibenKami'),
         SizedBox(
           height: defaultPadding,
         ),
-        Text('Skills',style: TextStyle(color: Colors.white),),
+        Text(
+          'Skills',
+          style: TextStyle(color: Colors.white),
+        ),
         SizedBox(
           height: defaultPadding,
         ),
